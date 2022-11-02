58c9b479809e8dc9f1a2be3d13eb4256e32e39b0
longzhaohui <longzhaohui@163.com>
2022-11-02 10:41:31 +0800

    fix

--- src/main/java/com/example/first/redis/RedisListenerConfig.java
+++ src/main/java/com/example/first/redis/RedisListenerConfig.java
@@ -1,17 +1,17 @@
-//package com.example.first.redis;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-//
-//@Configuration
-//public class RedisListenerConfig {
-//
-//    @Bean
-//    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory){
-//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        return container;
-//    }
-//}
+package com.example.first.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class RedisListenerConfig {
+
+    @Bean
+    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory){
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+}
