4c1f7628278cc5d13e02eb65a2830ae0a52f34b1
longzhaohui <longzhaohui@163.com>
2022-12-16 16:57:51 +0800

    贪吃蛇

---
+++ src/main/java/com/example/first/snake/Data.java
@@ -0,0 +1,23 @@
+package com.example.first.snake;
+
+import javax.swing.*;
+
+// 存储图片
+class Data {
+
+    public static ImageIcon body = new ImageIcon("image/img_body.png");
+
+    public static ImageIcon down = new ImageIcon("image/img_down.png");
+
+    public static ImageIcon food = new ImageIcon("image/img_food.png");
+
+    public static ImageIcon header = new ImageIcon("image/header.png");
+
+    public static ImageIcon left = new ImageIcon("image/img_left.png");
+
+    public static ImageIcon right = new ImageIcon("image/img_right.png");
+
+    public static ImageIcon up = new ImageIcon("image/img_up.png");
+
+}
+
